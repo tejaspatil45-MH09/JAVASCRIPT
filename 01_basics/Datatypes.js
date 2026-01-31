01c7e35aa66f1af91cd9029c8be8a1be022ad825
TejasPatil45 <tejas113128@gmail.com>
2026-01-31 19:25:23 +0000

    datatypes&variable

---
+++ 01_basics/Datatypes.js
@@ -0,0 +1,12 @@
+"Use strict";//treat js code as newer vesion
+
+//alert(2+3) //this will work in browser but not in node.js
+let name="tejas"
+let age=18
+
+let temp=null; //used when we dont know the value now and also we dont want it 0 or undeefined
+
+console.log(typeof age);
+
+//console.log(typeof null)=object
+//console.log(typeof undefined )=undefined
---
+++ 01_basics/variable.js
@@ -0,0 +1,10 @@
+console.log("i am tejas");
+const pi=3.14;
+let a=10;
+var b=14;
+student_name="tejas";
+
+console.log(pi);
+console.table([pi,a,b,student_name]);
+pi=31;
+
