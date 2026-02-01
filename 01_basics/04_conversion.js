df89a6cf1c7d293e318accc28246bbfbf2df1c00
TejasPatil45 <tejas113128@gmail.com>
2026-02-01 05:22:25 +0000

    we learn conversiondataye

---
+++ 01_basics/04_conversion.js
@@ -0,0 +1,22 @@
+"use strict"
+let name="22"
+
+/*console.log(typeof name)
+let intergerofname=Number(name)
+console.log(typeof intergerofname)
+console.log(intergerofname)
+
+//"33"=>33
+//"3teja"=>Nan
+//true=1 false=>0
+
+let issunday=true
+let sunday=Number(issunday)
+console.log(sunday)
+console.log(typeof sunday);*/
+
+let monday="tejas"
+let ismonday=Boolean(monday)
+console.log(ismonday);
+console.log(typeof ismonday);
+
